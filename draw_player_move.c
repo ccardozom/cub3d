b7af7d44aa8d122265fad2ffee813a99343d9691
Carlos Cardozo <54796906+ccardozom@users.noreply.github.com>
2020-09-24 13:47:15 +0200

    imprimir rayos prueba1

--- draw_player_move.c
+++ draw_player_move.c
@@ -17,23 +17,14 @@ void	draw_player_move(t_game *pos)
 	float	x;
 	float	y;
 	int 	i;
-	float	rayangle_init;
-	float	rayangle_finish;
 
 	y = pos->player.pos.y;
 	x = pos->player.pos.x;
-	rayangle_init = pos->player.player_angle - (pos->player.FOV_angle / 2);
-	rayangle_finish = pos->player.player_angle + (pos->player.FOV_angle / 2);
-	
-	while (rayangle_init < rayangle_finish)
+	i = 0;
+	while (i < pos->player.num_rays)
 	{
-		i = 0;
-		while (i < 50)
-		{
-			my_mlx_pixel_put(&pos->img, x + cos(rayangle_init) * i,
-			y + sin(rayangle_init) * i, 0x044D02);
-			i++;
-		}
-		rayangle_init += 0.1;
+		my_mlx_pixel_put(&pos->img, x + cos(pos->rays[i].rayangle) * pos->rays[i].wallhitX,
+		y + sin(pos->rays[i].rayangle) * pos->rays[i].wallhitY, 0x044D02);
+		i++;
 	}
 }
