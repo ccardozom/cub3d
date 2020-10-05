55758111ec5cbbd72382ba9da8e880d371114be5
Carlos Cardozo <54796906+ccardozom@users.noreply.github.com>
2020-10-05 13:40:04 +0200

    se prueba colision

--- raycast.c
+++ raycast.c
@@ -82,13 +82,13 @@ void	horizontal_intersection(t_game *pos)
 
 float	normalizeangle(float angle)
 {
-	// angle = remainder(angle, PI * 2);
-	// if (angle < 0)
-	// 	angle = PI * 2 + angle;
-	if (angle > 2 * PI)
-		angle -= 2 * PI;
+	angle = remainder(angle, PI * 2);
 	if (angle < 0)
-		angle += 2 * PI;
+		angle = (PI * 2) + angle;
+	// if (angle > 2 * PI)
+	// 	angle -= 2 * PI;
+	// if (angle < 0)
+	// 	angle += 2 * PI;
 	return (angle);
 }
 
@@ -103,9 +103,9 @@ void	castray(t_game *pos, t_ray *ray_data)
 	horizontal_intersection(pos);
 	vertical_intersection(pos);
 	pos->ray.horizhitdistance = pos->rays.foundhorizwallhit ? distancebetweenpoints(pos->player.pos.x,
-	pos->player.pos.y, pos->rays.horizwallhitx, pos->rays.horizwallhity) : 2147483647;
+	pos->player.pos.y, pos->rays.horizwallhitx, pos->rays.horizwallhity) : 1;
 	pos->ray.verthitdistance = pos->rays.foundvertwallhit ? distancebetweenpoints(pos->player.pos.x,
-	pos->player.pos.y, pos->rays.vertwallhitx, pos->rays.vertwallhity) : 2147483647;
+	pos->player.pos.y, pos->rays.vertwallhitx, pos->rays.vertwallhity) : 1;
 	if (pos->ray.verthitdistance < pos->ray.horizhitdistance)
 	{
 		ray_data->distance = pos->ray.verthitdistance;
